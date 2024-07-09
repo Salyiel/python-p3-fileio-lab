c599b79db8f996ff452a5d60c1676e70483ae940
Salyiel <162126755+Salyiel@users.noreply.github.com>
2024-07-09 15:53:20 +0300

    Writefile code

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,7 +1,15 @@
 def write_file(file_name, file_content):
+    # Ensure the file has a .txt extension
+    if not file_name.endswith('.txt'):
+        file_name += '.txt'
+    
+    # Write the content to the file
+    with open(file_name, 'w') as file:
+        file.write(file_content)
     pass
 
 def append_file(file_name, append_content):
+    
     pass
 
 def read_file(file_name):
